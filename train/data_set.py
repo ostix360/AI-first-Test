73180371b75d5c1979509a0d09902132a3af2fac
ostix360 <55257054+ostix360@users.noreply.github.com>
2023-03-18 10:43:51 +0000

    length cap for each batch

--- train/data_set.py
+++ train/data_set.py
@@ -1,11 +1,13 @@
 import torch
-from transformers import AdamW, AutoTokenizer, AutoModelForSequenceClassification, BertModel
+from transformers import AdamW, AutoTokenizer, AutoModelForSequenceClassification, BertModel, DataCollatorWithPadding
 from datasets import load_dataset
 
 # Même chose que précédemment
 checkpoint = "bert-base-uncased"
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-model = BertModel.from_pretrained(checkpoint)
+#model = BertModel.from_pretrained(checkpoint)
+
+#print(torch.cuda.is_available())
 
 
 def tokenize_function(examples):
@@ -19,10 +21,19 @@ raw_datasets = load_dataset("glue", "mrpc")
 raw_train_data = raw_datasets["train"]
 raw_validation_data = raw_datasets["validation"]
 
-tokenized_sentence1 = tokenizer(raw_train_data[15]["sentence1"], raw_train_data[15]["sentence2"], padding=True,
-                                truncation=True)
+#tokenized_sentence1 = tokenizer(raw_train_data[15]["sentence1"], raw_train_data[15]["sentence2"], padding=True,
+#                                truncation=True)
+
 tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
-print(tokenized_datasets)
+
+data_colator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+samples = tokenized_datasets["train"][:8]
+samples = {k: v for k, v in samples.items() if k not in ["idx", "sentence1", "sentence2"]}
+print([len(x) for x in samples["input_ids"]])   # print the length of each sample max length is 67
+batch = data_colator(samples)                # collate the samples into a batch with the maximum length of 67
+print({k: v.shape for k, v in batch.items()})  # print the shape of each tensor in the batch 
+
 
 # sequences = [                                  TODO : set Env var HF_HOME=/mnt/d/DataScience/HG_cache
 #     "I've been waiting for a HuggingFace course my whole life.",
