3e678bf4258294ef7e1009030c3e20d6be88aec3
ostix360 <vicrobin1001@gmail.com>
2023-03-19 13:06:54 +0100

    own trainer done + accelerate API started

--- train/my_trainer.py
+++ train/my_trainer.py
@@ -1,4 +1,5 @@
 from datasets import load_dataset
+from torch.optim import AdamW
 from torch.utils.data import DataLoader
 from transformers import AutoTokenizer, DataCollatorWithPadding, AutoModelForSequenceClassification
 
@@ -15,23 +16,27 @@ def tokenize_function(examples):
 
 
 tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
-
 final_tokenized_datasets = utils.datasets_post_process(tokenized_datasets)
-
 data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
 train_dataloader = DataLoader(
     final_tokenized_datasets["train"], batch_size=8, shuffle=True, collate_fn=data_collator
 )
-
 eval_dataloader = DataLoader(
     final_tokenized_datasets["validation"], batch_size=8, collate_fn=data_collator
 )
 
-batch = utils.debug_data_processing(train_dataloader)
-
-outputs = model(**batch)
-
 # Debug
-print(outputs.loss, outputs.logits.shape)
+# batch = utils.debug_data_processing(train_dataloader)
+# outputs = model(**batch)
+# print(outputs.loss, outputs.logits.shape)
 
+device = utils.get_device()
+optimizer = AdamW(model.parameters(), lr=3e-5)
+model.to(device)
+num_epochs = 1
+lr_scheduler, num_training_steps = utils.get_lr_scheduler(optimizer, train_dataloader, num_epochs)
+
+utils.train_model(model, train_dataloader, num_epochs, optimizer, lr_scheduler, device, num_training_steps)
+evaluation = utils.evaluate_model(model, eval_dataloader, device)
+print(evaluation)
